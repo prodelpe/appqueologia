ebbd4cd2889ebde9d7bc7e866becf590c7388ee1
prodelpe <prodelpe@gmail.com>
2020-11-20 21:54:33 +0100

    Amaguem Register i login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,9 +49,11 @@
                         <ul class="navbar-nav ml-auto">
                             <!-- Authentication Links -->
                             @guest
+                            @if(false)
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
+                            @endif
                             @if (Route::has('register'))
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
