3bb3d7b1a4ac5d845d5a218c5bb92599a24d51cd
prodelpe <prodelpe@gmail.com>
2020-11-20 21:46:52 +0100

    Correció Error Auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,11 +108,11 @@
         <!-- Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-
+        {{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>--}}        
+                    
         <!-- Vue.js -->
-        <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script><!-- DEV -->
-        <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2"></script> PRODUCTION -->
+        {{--<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script><!-- DEV -->--}}
+        <script src="https://cdn.jsdelivr.net/npm/vue@2"></script><!-- PRODUCTION -->
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
